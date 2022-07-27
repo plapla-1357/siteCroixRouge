24132dc5f279f9e50ffd148e368d0bfc2049854a
Gaëtan <73687455+plapla-1357@users.noreply.github.com>
2022-07-27 12:04:17 +0200

    page d'accueille corect

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+// let nav_elements = document.querySelectorAll('.nav_element');
+// nav_elements.forEach(function(element) {
+//     element.addEventListener('click', function(event) {
+//         let menus = element.getElementsByClassName("menu")
+//     })
+// })
