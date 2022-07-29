c5e71798efa2c2029826ab9365fe529775fd5841
Gaëtan <73687455+plapla-1357@users.noreply.github.com>
2022-07-29 09:09:00 +0200

    nav fonctionelle (!! probleme d'UI)

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,40 @@
-// let nav_elements = document.querySelectorAll('.nav_element');
-// nav_elements.forEach(function(element) {
-//     element.addEventListener('click', function(event) {
-//         let menus = element.getElementsByClassName("menu")
-//     })
-// })
+console.log("hello")
+let img_landingpage = document.getElementsByClassName("img_landingpage")
+let descriptions = document.getElementsByClassName("titre")
+let selector = document.getElementById("selector")
+
+let landingpage = document.getElementById("landingpage")
+
+let paused = false;
+let toogle_pause = () => {
+    if (!paused){
+        
+        for (let i = 0; i < img_landingpage.length; i++) {
+            console.log(img_landingpage[i])
+            img_landingpage[i].style.animationPlayState = "paused"
+        }
+        for (let i = 0; i < descriptions.length; i++) {
+            console.log(descriptions[i].querySelector('h1'))
+            descriptions[i].querySelector('h1').style.animationPlayState = "paused"
+            descriptions[i].querySelector('p').style.animationPlayState = "paused"
+            descriptions[i].querySelector('.button').style.animationPlayState = "paused"
+            
+        }
+        selector.style.animationPlayState = "paused"
+        paused = true;
+    }else{
+        for (let i = 0; i < img_landingpage.length; i++) {
+            console.log(img_landingpage[i])
+            img_landingpage[i].style.animationPlayState = "running"
+        }
+        for (let i = 0; i < descriptions.length; i++) {
+            console.log(descriptions[i].querySelector('h1'))
+            descriptions[i].querySelector('h1').style.animationPlayState = "running"
+            descriptions[i].querySelector('p').style.animationPlayState = "running"
+            descriptions[i].querySelector('.button').style.animationPlayState = "running"
+            
+        }
+        selector.style.animationPlayState = 'running'
+        paused = false;   
+    }
+}
---
+++ src/js/nav.js
@@ -0,0 +1,34 @@
+let sous_navs_elements = document.querySelectorAll(".sous_nav_element")
+console.log(sous_navs_elements)
+let all_nav_elements = document.querySelectorAll(".nav_element")
+
+sous_navs_elements.forEach(element => {
+    element.addEventListener('click', function(){
+        
+        let a = element.querySelector('.sous_menu')
+        if (a.classList.contains('open')){
+            a.parentElement.classList.add('closed')
+        }
+        a.classList.toggle('open')
+        a.parentElement.querySelector('.element_title').classList.toggle('arrow')
+        
+        let all_sous_menu = element.parentElement.parentElement.querySelectorAll('.sous_menu:not(.open)')
+        console.log(all_sous_menu)
+        for (let i = 0; i < all_sous_menu.length; i++){
+            all_sous_menu[i].parentElement.classList.toggle('closed')
+            all_sous_menu[i].classList.remove('open')          
+        }
+    })
+});
+
+all_nav_elements.forEach(function(element){
+    element.addEventListener('mouseleave', function(){
+        console.log("hoverout")
+        let all_sous_menu = element.querySelectorAll('.sous_menu')
+        for (let i = 0; i < all_sous_menu.length; i++){
+            all_sous_menu[i].classList.remove('open')
+            all_sous_menu[i].parentElement.classList.remove('closed')
+            all_sous_menu[i].parentElement.querySelector('.element_title').classList.remove('arrow')
+        }
+    })
+})
